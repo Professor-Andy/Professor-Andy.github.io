e058b5729b287e87dee48c1c35fe8ef0a3d5d251
Professor-Andy <Austin.Anderson@stmartin.edu>
2026-03-29 00:41:44 +0000

    asdgf

--- scripts/Navigation.js
+++ scripts/Navigation.js
@@ -14,26 +14,7 @@ function Navigation(id, pagePath) {
     fetch(pagePath)
         .then(res => res.text())
         .then(html => {
-            const page = document.getElementById("Page");
-            page.innerHTML = html;
-
-            // reinitialize your carousel
-            initCarousel();
-
-            // ⭐ ONLY add the YouTube iframe when loading the Info page
-            if (id === "info") {
-                const iframe = document.createElement("iframe");
-                iframe.width = "75%";
-                iframe.height = "400";
-                iframe.src = "https://www.youtube-nocookie.com/embed/Vs1ljw5JPZ8";
-                iframe.title = "YouTube video player";
-                iframe.frameBorder = "0";
-                iframe.allow =
-                    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-                iframe.allowFullscreen = true;
-
-                // append it at the bottom of the Info page
-                page.appendChild(iframe);
-            }
+            document.getElementById("Page").innerHTML = html;
+	    	initCarousel();
         });
 }
